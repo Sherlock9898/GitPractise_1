eb3a5ec6e6bee870d19caae012243c08c2690484
Jakadef <jakadef507@gmail.com>
2023-05-13 09:48:38 +0500

    work 4 is done

--- src/day1/Second.java
+++ src/day1/Second.java
@@ -1,9 +1,13 @@
 package day1;
 
 public class Second {
+    public static void main(String[] args) {
+
+        System.out.println("Hi Hello");
 // work 1 is done here
-    // work 2 is done here
-    // work 3 is done here
-    // work 4 is done here
-    // work 5 is done here
+        // work 2 is done here
+        // work 3 is done here
+        // work 4 is done here
+        // work 5 is done here
+    }
 }
