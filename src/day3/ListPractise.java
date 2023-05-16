2b4f1a05d0559d19360e89a4d8f7649a44f916cc
Jakadef <jakadef507@gmail.com>
2023-05-16 14:04:10 +0500

    adding few items

---
+++ src/day3/ListPractise.java
@@ -0,0 +1,13 @@
+package day3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListPractise {
+
+    public static void main(String[] args) {
+
+        List<String> lst = new ArrayList<>();
+        // adding one item
+    }
+}
