ad4c347d0627f2e8daa083b25a6a20647afbf074
Jakadef <jakadef507@gmail.com>
2023-05-16 14:17:27 +0500

    feature of set

--- src/day3/SetPractise.java
+++ src/day3/SetPractise.java
@@ -2,7 +2,7 @@ package day3;
 
 public class SetPractise {
 // Set is a collection type that only store unique item
-    // if you add duplicate
+    // if you add duplicate it will fust remove it
     // It's a collection of unique elements
 
 }
