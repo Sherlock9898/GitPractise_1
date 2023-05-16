ae7cdde282e42f7956c0fd8aa6edce1c20969c4f
Jakadef <jakadef507@gmail.com>
2023-05-16 13:59:53 +0500

    Test case part 5 is done

--- src/day1/ResetPractise.java
+++ src/day1/ResetPractise.java
@@ -4,4 +4,10 @@ public class ResetPractise {
     // part 1 is done
     // part 2 is done
     // part 3 is done
+    // part 4 is done
+    // part 5 is done
+    // Mixed | Soft rest remove commit from history but keep the local changes
+    // Did part 4 part 5 and I am done in one commit
+
+
 }
